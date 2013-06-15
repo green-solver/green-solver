adf492a59737ba0e6236095951c10fdbcce6dbaa
jacogeld <jacogeld@dc7788dd-e2b1-b8d1-6a2f-f892e6f9de28>
2013-06-15 00:26:53 +0000

    Added tests for Z3.

--- trunk/green/test/za/ac/sun/cs/green/EntireSuite.java
+++ trunk/green/test/za/ac/sun/cs/green/EntireSuite.java
@@ -8,6 +8,7 @@ import za.ac.sun.cs.green.service.choco.SATChocoTest;
 import za.ac.sun.cs.green.service.cvc3.SATCVC3Test;
 import za.ac.sun.cs.green.service.slicer.ParallelSATSlicerTest;
 import za.ac.sun.cs.green.service.slicer.SATSlicerTest;
+import za.ac.sun.cs.green.service.z3.SATZ3Test;
 import za.ac.sun.cs.green.util.ParallelSATTest;
 import za.ac.sun.cs.green.util.SetServiceTest;
 import za.ac.sun.cs.green.util.SetTaskManagerTest;
@@ -22,7 +23,8 @@ import za.ac.sun.cs.green.util.SetTaskManagerTest;
 	SATChocoTest.class,
 	SATCVC3Test.class,
 	ParallelSATSlicerTest.class,
-	ParallelSATTest.class
+	ParallelSATTest.class,
+	SATZ3Test.class
 })
 
 public class EntireSuite {
