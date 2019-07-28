0ce6cd21a57d0b344cb1f7d041aebe5f604ce480
Jaco Geldenhuys <jacogeld@gmail.com>
2019-07-28 06:29:17 +0200

    Deleted files

--- green/src/za/ac/sun/cs/green/expr/Constant.java
+++
@@ -1,5 +0,0 @@
-package za.ac.sun.cs.green.expr;
-
-public abstract class Constant extends Expression {
-
-}
--- green/src/za/ac/sun/cs/green/util/Reporter.java
+++
@@ -1,7 +0,0 @@
-package za.ac.sun.cs.green.util;
-
-public interface Reporter {
-
-	public void report(String context, String message);
-
-}
